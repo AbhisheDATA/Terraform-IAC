abd4d6147eea9e136b813bb2fb62bdbd0a60c62d
Abhishek <abhisheklinkd5@gmail.com>
2024-12-29 18:55:39 +0530

    file updaated

--- versions.tf
+++ versions.tf
@@ -1,13 +1,6 @@
 terraform {
   required_version = ">= 1.0.0"
   
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 4.0"
-    }
-  }
-
   cloud {
     workspaces {
       name = "Terraform-IAC"
