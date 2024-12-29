7d18b2c837be7e46f9c2f8acdec5adefec9cb1f2
Abhishek <abhisheklinkd5@gmail.com>
2024-12-29 18:46:24 +0530

    update in main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,16 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+}
+
 provider "google" {
-  project = var.project_id
-  region  = var.region
+  project     = var.project_id
+  region      = var.region
+  credentials = file("service-account-key.json")
 }
 
 # Create GCP Project
